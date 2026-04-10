de3817bb14b210f529bdb0732aa3ca74915a1059
Dev <113640546+Dev2kod@users.noreply.github.com>
2026-04-10 03:43:48 +0000

    nig

--- ViolationSearch/app/main.py
+++ ViolationSearch/app/main.py
@@ -1,6 +1,12 @@
 from fastapi import FastAPI
 from .api.search import router as searchRouter
 from fastapi.middleware.cors import CORSMiddleware
+from sqlalchemy import create_engine, text
+
+Database_URL = "sqlite:///D:\\safety-monitor\\ViolationSearch\\app\\violations.db"
+engine = create_engine(Database_URL, echo=True)
+    
+
 # from app.core.llmParser import testLlm
 app = FastAPI()
 
@@ -12,6 +18,19 @@ app.add_middleware(
     allow_headers=["*"],
 )
 app.include_router(searchRouter)
+
+@app.get("/violations")
+def get_violations():
+    
+    query = "SELECT v.Violation_ID AS id,v.TimeStamp,v.Camera_ID,v.Severity,GROUP_CONCAT(d.ViolationType_ID) AS violation_type_ids FROM Violations v JOIN ViolationDetails d ON v.Violation_ID = d.Violation_ID GROUP BY v.Violation_ID, v.TimeStamp, v.Camera_ID, v.Severity;"
+
+    try:
+        with engine.connect() as conn:
+            result = conn.execute(text(query))
+            data = [dict(row._mapping) for row in result]
+        return {"data": data}
+    except Exception as e:
+        return {"error": str(e)}
 
 @app.get("/")
 def home():
